e8335aa16464026fd4573852eb93c15c35e7d5ab
jandreu <jandreu@student.42.fr>
2019-04-30 11:11:07 +0200

    mise a la norme de common/create_socket.c

--- common/create_socket.c
+++ common/create_socket.c
@@ -1,45 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_socket.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jandreu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/30 11:04:16 by jandreu           #+#    #+#             */
+/*   Updated: 2019/04/30 11:04:19 by jandreu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "../includes/ft_p.h"
 
-int 					create_server(int port)
+int						create_server(int port)
 {
-	int 				sock;
+	int					sock;
 	struct protoent		*proto;
 	struct sockaddr_in	sin;
 
 	proto = getprotobyname("tcp");
 	if (proto == 0)
-		return -1;
-	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
-	{
-		printf("ERROR\n");
-		return -1;
-	}
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(port);
-	sin.sin_addr.s_addr = htonl(INADDR_ANY);	// on accepte n'importe quelle adresse
-	if (bind(sock, (const struct sockaddr*)&sin, sizeof(sin)) == -1)
-	{
-		printf("Bind error");
-		exit(EXIT_FAILURE);
-	}
-	listen(sock, 2);	// limite le nombre de connexions simultanée
-	return(sock);
-}
-
-int 					create_client(char *addr, int port)
-{
-	int 				sock;
-	struct protoent		*proto;
-	struct sockaddr_in	sin;
-
-	proto = getprotobyname("tcp");
-	if (proto == 0)
-		return -1;
+		return (-1);
 	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
 	{
 		print_error("Erreur dans la création de la socket");
-		return -1;
+		return (-1);
+	}
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (const struct sockaddr*)&sin, sizeof(sin)) == -1)
+	{
+		print_error("Erreur dans le bind de la socket");
+		exit(EXIT_FAILURE);
+	}
+	listen(sock, 2);
+	return (sock);
+}
+
+int						create_client(char *addr, int port)
+{
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	proto = getprotobyname("tcp");
+	if (proto == 0)
+		return (-1);
+	if ((sock = socket(PF_INET, SOCK_STREAM, proto->p_proto)) == -1)
+	{
+		print_error("Erreur dans la création de la socket");
+		return (-1);
 	}
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(port);
@@ -49,5 +60,5 @@ int 					create_client(char *addr, int port)
 		print_error("Erreur dans la connection a la socket");
 		exit(EXIT_FAILURE);
 	}
-	return(sock);
+	return (sock);
 }
